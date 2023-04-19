e0edb58e921919917526c4a2c150802dfe9a5326
Yoav Klein <yoavklein25@gmail.com>
2023-04-19 19:34:43 +0000

    App images in directory

---
+++ app-images/generator/generator-v2.py
@@ -0,0 +1,13 @@
+
+import random
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return f"{random.randint(100, 1000)}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
